5c4938e4b81a4b2af50a31e1617258f18b81e343
Eric Schrock <eric.schrock22@gmail.com>
2020-01-19 01:33:33 +0000

    Lidar lesson 4 section 9 (challenge problem)

--- Lidar_Obstacle_Detection/src/environment.cpp
+++ Lidar_Obstacle_Detection/src/environment.cpp
@@ -189,6 +189,7 @@ int main (int argc, char** argv)
 
     ProcessPointClouds<pcl::PointXYZI> * pointProcessorI = new ProcessPointClouds<pcl::PointXYZI>();
     std::vector<boost::filesystem::path> stream = pointProcessorI->streamPcd("../src/sensors/data/pcd/data_1");
+    //std::vector<boost::filesystem::path> stream = pointProcessorI->streamPcd("../src/sensors/data/pcd/data_2"); // Challenge problem data (bicyclist)
     auto streamIterator = stream.begin();
     pcl::PointCloud<pcl::PointXYZI>::Ptr inputCloudI;
 
